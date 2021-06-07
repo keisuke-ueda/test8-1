704f195acc0bf469c91a4e0af7b7fc637c456932
keisuke-ueda <keisuke.ueda@field-up.work>
2021-06-07 11:29:57 +0900

    二度目

--- resources/views/works.blade.php
+++ resources/views/works.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>記事一覧</title>
-    <h1>掃除</h1>
+    <h1>掃除する</h1>
 </head>
 <body>
     
